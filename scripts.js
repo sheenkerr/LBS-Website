5b77eda4b8b6c7019b35013df7851800ddcbbe54
Sheen Kerr <sheenkerr@gmail.com>
2021-06-03 17:06:45 +0800

    Edited Footer CSS

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 // Mobile Menu
 function HamburgerMenu() {
   document.getElementById("mobile-menu").style.display = "block";
-  document.querySelector("footer").style = "display:none";
+  // document.querySelector("footer").style = "display:none";
   document.querySelector("main").style = "display:none";
 }
 function CloseHamburgerMenu() {
   document.getElementById("mobile-menu").style.display = "none";
   document.querySelector("main").style = "display:block";
-  document.querySelector("footer").style = "display:block";
+  // document.querySelector("footer").style = "display:block";
 }
 
 // Form Submission
