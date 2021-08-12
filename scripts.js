cb22e5b5ccf8164da72f8d9d22aca67ccaa139c9
Sheen Kerr <sheenkerr@gmail.com>
2021-08-12 20:42:24 +0800

    Added hide footer in js for mobile view

--- scripts.js
+++ scripts.js
@@ -9,13 +9,13 @@ gtag('config', 'G-W9TP9BL82P');
 // Mobile Menu
 function HamburgerMenu() {
   document.getElementById("mobile-menu").style.display = "block";
-  // document.querySelector("footer").style = "display:none";
+  document.querySelector("footer").style = "display:none";
   document.querySelector("main").style = "display:none";
 }
 function CloseHamburgerMenu() {
   document.getElementById("mobile-menu").style.display = "none";
   document.querySelector("main").style = "display:block";
-  // document.querySelector("footer").style = "display:block";
+  document.querySelector("footer").style = "display:block";
 }
 
 // Form Submission
